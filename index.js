9a247e83d5dffccec0b00d5b2097a8714c66a2d2
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-14 17:41:17 +0300

    Create class Student

--- index.js
+++ index.js
@@ -40,3 +40,45 @@ class User {
     }
 }
 
+class Student extends User {
+    /**
+     *
+     * @param {string} name
+     * @param {string} surname
+     * @param {number} year
+     */
+    constructor(name, surname, year) {
+        super(name, surname);
+        this._year = year;
+    }
+
+    set year(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Users year must be number type');
+        }
+
+        this._year = value;
+    }
+
+    get year() {
+        return this._year;
+    }
+
+    getCourse() {
+        const currentYear = new Date().getFullYear();
+        const course = currentYear - this._year;
+
+        if (course > 5) {
+            throw new RangeError('This student already graduated');
+        }
+        if (course < 1) {
+            throw new RangeError('This student has not entered yet');
+        }
+
+        return course;
+    }
+}
+
+const ivan = new Student('Ivan', 'Ivanov', 2016);
+console.log(ivan.getFullName());
+console.log(ivan.getCourse());
